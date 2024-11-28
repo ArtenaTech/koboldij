8d1b5b6c315a22c6f277a826bb2a177df0feaf47
ArtenaTech <litvinoff.natalia@gmail.com>
2024-11-28 12:53:03 +0300

    Create PluginIcons.java

---
+++ src/main/java/icons/PluginIcons.java
@@ -0,0 +1,11 @@
+package icons;
+
+import com.intellij.openapi.util.IconLoader;
+
+import javax.swing.*;
+
+public class PluginIcons {
+
+    public static final Icon popup_icon = IconLoader.getIcon("/icons/llm_16.svg", PluginIcons.class);
+
+}
